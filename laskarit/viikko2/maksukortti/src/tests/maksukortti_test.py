c3c8df8884732690c53a3757ff4e3aeb5733739e
Kasper Envalds <kasper.envalds@gmail.com>
2022-03-28 17:19:05 +0300

    Unicafe haettu

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -1,6 +1,7 @@
 import unittest
 from maksukortti import Maksukortti
 
+
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.kortti = Maksukortti(10)
@@ -10,12 +11,11 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
         self.kortti.syo_edullisesti()
-        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.5 euroa")
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.5 euroa",)
 
     def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
         self.kortti.syo_maukkaasti()
         self.assertEqual(str(self.kortti), "Kortilla on rahaa 6 euroa")
-
 
     def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
         self.kortti.syo_maukkaasti()
@@ -31,8 +31,12 @@ class TestMaksukortti(unittest.TestCase):
         self.kortti.lataa_rahaa(200)
         self.assertEqual(str(self.kortti), "Kortilla on rahaa 150 euroa")
 
-    def test_syo_maukkaasti_ei_vie_saldo_negatiiviseksi(self):
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
         self.kortti.syo_maukkaasti()
         self.kortti.syo_maukkaasti()
         self.kortti.syo_maukkaasti()
         self.assertEqual(str(self.kortti), "Kortilla on rahaa 2 euroa")
+
+    def test_kortille_ei_voi_ladata_negatiivista_summaa(self):
+        self.kortti.lataa_rahaa(-25)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10 euroa") 
